1a7018e173771ae43b5a23047f6672f8b37b24b0
USER <USER@DESKTOP-GL2K8I4>
2023-02-24 09:56:16 +0530

    Add project files.

---
+++ Emailsending/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emailsending
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Random rn = new Random();
+                int otp = rn.Next(1000, 8000);
+
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress("saranyasasi841@gmail.com");
+
+                mail.To.Add("chin929@gmail.com");
+                mail.Priority = MailPriority.High;
+                mail.Subject = "Test Email";
+                mail.Body =otp.ToString();
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("saranyasasi841@gmail.com", "hwyzliqmtdsrgcvu"); // token correct ?
+                SmtpServer.EnableSsl = true;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.Send(mail);
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            Console.WriteLine("Email Send Succesfully");
+
+        }
+    }
+
+}
+  
+
